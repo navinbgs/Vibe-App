166c1282894dbbe62b9376daee229bd130ace175
Navin Bgs <navinbgs@hotmail.com>
2026-02-23 10:23:06 -0500

    Fix root route

--- index.js
+++ index.js
@@ -5,21 +5,29 @@ const serverless = require("serverless-http");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.json());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
 
 let entries = [];
+
+// ROOT ROUTE (IMPORTANT)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // API endpoint
 app.get("/entries", (req, res) => {
   res.json(entries);
 });
 
+// Handle form submission
 app.post("/add", (req, res) => {
   const newEntry = req.body.entry;
   entries.push(newEntry);
   res.redirect("/");
 });
 
-// Export instead of listen()
 module.exports = app;
 module.exports.handler = serverless(app);
